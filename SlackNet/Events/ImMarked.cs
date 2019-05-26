d7828c0ff4d55c78fbd29fff66b649c556a50e1b
Simon Oxtoby <simonoxtoby@gmail.com>
2019-05-26 13:12:23 +1000

    Updating dictionary and fixing typo

--- SlackNet/Events/ImMarked.cs
+++ SlackNet/Events/ImMarked.cs
@@ -3,7 +3,7 @@ namespace SlackNet.Events
     /// <summary>
     /// Sent to all open connections for a user when that user moves the read cursor in a direct message channel.
     /// </summary>
-    public class ImMaarked : Event
+    public class ImMarked : Event
     {
         public string Channel { get; set; }
         public string Ts { get; set; }
