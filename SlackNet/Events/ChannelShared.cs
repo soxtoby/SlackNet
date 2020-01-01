4de9168ce278fcded0cae2c8a75eed8a3e4101ff
Simon Oxtoby <simonoxtoby@gmail.com>
2020-01-01 13:55:07 +1000

    Adding channel shared/unshared events

---
+++ SlackNet/Events/ChannelShared.cs
@@ -0,0 +1,19 @@
+﻿namespace SlackNet.Events
+{
+    /// <summary>
+    /// Sent to all event subscriptions when a new shared channel is created or a channel is converted into a shared channel.
+    /// It's also triggered when an external workspace is added to an existing shared channel.
+    /// </summary>
+    public class ChannelShared : Event
+    {
+        /// <summary>
+        /// The team ID of the workspace that has joined the channel. 
+        /// </summary>
+        public string ConnectedTeamId { get; set; }
+        /// <summary>
+        /// The ID for the public or private channel.
+        /// </summary>
+        public string Channel { get; set; }
+        public string EventTs { get; set; }
+    }
+}
---
+++ SlackNet/Events/ChannelUnshared.cs
@@ -0,0 +1,22 @@
+﻿namespace SlackNet.Events
+{
+    /// <summary>
+    /// Sent to all event subscriptions when an external workspace has been removed from an existing shared channel.
+    /// </summary>
+    public class ChannelUnshared : Event
+    {
+        /// <summary>
+        /// The team ID of the workspace that has been removed from the channel.
+        /// </summary>
+        public string PreviouslyConnectedTeamId { get; set; }
+        /// <summary>
+        /// The ID for the public or private channel.
+        /// </summary>
+        public string Channel { get; set; }
+        /// <summary>
+        /// True if the channel is still externally shared, and False otherwise.
+        /// </summary>
+        public bool IsExtShared { get; set; }
+        public string EventTs { get; set; }
+    }
+}
