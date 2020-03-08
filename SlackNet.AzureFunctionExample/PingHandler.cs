9ed16b1e055f88d7645b394594a73680725af4de
Simon Oxtoby <simonoxtoby@gmail.com>
2020-03-08 14:45:56 +1000

    Adding AzureFunctionExample project

---
+++ SlackNet.AzureFunctionExample/PingHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using SlackNet.Events;
+using SlackNet.WebApi;
+
+namespace SlackNet.AzureFunctionExample
+{
+    public class PingHandler : IEventHandler<MessageEvent>
+    {
+        private static int _count;
+
+        private readonly ISlackApiClient _slack;
+        public PingHandler(ISlackApiClient slack) => _slack = slack;
+
+        public async Task Handle(MessageEvent slackEvent)
+        {
+            if (slackEvent.Text.Contains("ping"))
+                await _slack.Chat.PostMessage(new Message
+                    {
+                        Text = "pong",
+                        Attachments = { new Attachment { Text = $"Count: {++_count}" } },
+                        Channel = slackEvent.Channel
+                    });
+        }
+    }
+}
---
+++ SlackNet.AzureFunctionExample/SlackEndpoints.cs
@@ -0,0 +1,51 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using SlackNet.AspNetCore;
+
+namespace SlackNet.AzureFunctionExample
+{
+    public class SlackEndpoints
+    {
+        private readonly ISlackRequestHandler _requestHandler;
+        private readonly SlackJsonSettings _jsonSettings;
+        private readonly SlackEndpointConfiguration _endpointConfig;
+
+        public SlackEndpoints(ISlackRequestHandler requestHandler, SlackJsonSettings jsonSettings, SlackEndpointConfiguration endpointConfig)
+        {
+            _requestHandler = requestHandler;
+            _jsonSettings = jsonSettings;
+            _endpointConfig = endpointConfig;
+        }
+
+        [FunctionName("event")]
+        public async Task<IActionResult> Event([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequest request)
+        {
+            return SlackResponse(await _requestHandler.HandleEventRequest(request, _endpointConfig));
+        }
+
+        [FunctionName("action")]
+        public async Task<IActionResult> Action([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequest request)
+        {
+            return SlackResponse(await _requestHandler.HandleActionRequest(request, _endpointConfig));
+        }
+
+        [FunctionName("options")]
+        public async Task<IActionResult> Options([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequest request)
+        {
+            return SlackResponse(await _requestHandler.HandleOptionsRequest(request, _endpointConfig));
+        }
+
+        private ContentResult SlackResponse(SlackResponse response)
+        {
+            return new ContentResult
+                {
+                    StatusCode = (int)response.Status,
+                    ContentType = response.ContentType,
+                    Content = response.Body(_jsonSettings)
+                };
+        }
+    }
+}
