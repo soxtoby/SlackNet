3d36fe5f55ac9ceefe426dc4decfad586d02a4c9
Ben <benjamin.schuedzig@mantro.net>
2020-04-02 21:48:16 +0000

    Add missing fields in User entity (#36)

--- SlackNet/Objects/User.cs
+++ SlackNet/Objects/User.cs
@@ -9,18 +9,27 @@ namespace SlackNet
         public string Name { get; set; }
         public bool Deleted { get; set; }
         public string Color { get; set; }
+        public string RealName { get; set; }
+        public string Tz { get; set; }
+        public string TzLabel { get; set; }
+        public int TzOffset { get; set; }
         public UserProfile Profile { get; set; }
         public bool IsAdmin { get; set; }
         public bool IsOwner { get; set; }
         public bool IsPrimaryOwner { get; set; }
         public bool IsRestricted { get; set; }
         public bool IsUltraRestricted { get; set; }
+        public bool IsBot { get; set; }
+        public bool IsStranger { get; set; }
         public int Updated { get; set; }
+        public bool IsAppUser { get; set; }
+        public bool IsInvitedUser { get; set; }
         [JsonIgnore]
         public DateTime UpdateTime => Updated.ToDateTime().GetValueOrDefault();
         [JsonProperty("has_2fa")]
         public bool Has2FA { get; set; }
         public string TwoFactorType { get; set; }
+        public string Locale { get; set; }
         public EnterpriseUser EnterpriseUser { get; set; }
 
         public override string ToString() => Link.User(Id).ToString();
