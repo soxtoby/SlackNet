deb08628af73d0a8dd18683b28f9a010b184d292
Simon Oxtoby <soxtoby@zaptechnology.com>
2020-04-26 10:09:26 +1000

    Adding Respond extension method for responding to slash commands

--- SlackNet/SlackApiClientExtensions.cs
+++ SlackNet/SlackApiClientExtensions.cs
@@ -6,9 +6,10 @@ namespace SlackNet
 {
     public static class SlackApiClientExtensions
     {
-        public static Task Respond(this ISlackApiClient client, InteractionRequest request, MessageResponse response, CancellationToken? cancellationToken = null)
-        {
-            return client.Respond(request.ResponseUrl, new MessageUpdateResponse(response), cancellationToken);
-        }
+        public static Task Respond(this ISlackApiClient client, InteractionRequest request, MessageResponse response, CancellationToken? cancellationToken = null) => 
+            client.Respond(request.ResponseUrl, new MessageUpdateResponse(response), cancellationToken);
+
+        public static Task Respond(this ISlackApiClient client, SlashCommand slashCommand, SlashCommandResponse response, CancellationToken? cancellationToken = null) => 
+            client.Respond(slashCommand.ResponseUrl, new SlashCommandMessageResponse(response), cancellationToken);
     }
 }
