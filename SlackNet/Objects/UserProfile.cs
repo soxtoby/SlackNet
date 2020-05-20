444aebaadd49d61bf5761cfde25f785c23f43ab6
Filip Stojanac <22033305+fstojanac@users.noreply.github.com>
2020-05-20 02:07:44 +0200

    Add missing user properties
    * [ENH] Add missing properties to User and UserProfile types
    * [ENH] Add missing user profile property

--- SlackNet/Objects/UserProfile.cs
+++ SlackNet/Objects/UserProfile.cs
@@ -5,6 +5,7 @@ namespace SlackNet
 {
     public class UserProfile
     {
+        public string Title { get; set; }
         public string AvatarHash { get; set; }
         public string StatusEmoji { get; set; }
         public string StatusText { get; set; }
@@ -12,10 +13,14 @@ namespace SlackNet
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string RealName { get; set; }
+        public string RealNameNormalized { get; set; }
+        public string DisplayName { get; set; }
         public string Email { get; set; }
         public string Skype { get; set; }
         public string Phone { get; set; }
-        public bool AlwaysActive { get; set; }
+        public bool? AlwaysActive { get; set; }
+        public string Team { get; set; }
+        public string ImageOriginal { get; set; }
         [JsonProperty("image_24")]
         public string Image24 { get; set; }
         [JsonProperty("image_32")]
