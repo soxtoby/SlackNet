1cdaedf0fa74deb319131a3f1c8074b9811cfc27
Simon Oxtoby <soxtoby@zaptechnology.com>
2020-08-01 11:06:00 +1000

    Adding automatic delay and retry to Http for 429 (Too Many Requests) responses
    Resolves #58

--- SlackNet/Http.cs
+++ SlackNet/Http.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
@@ -15,20 +16,33 @@ namespace SlackNet
     {
         private readonly HttpClient _client;
         private readonly SlackJsonSettings _jsonSettings;
+        private readonly bool _retryOnRateLimit;
 
-        public Http(HttpClient client, SlackJsonSettings jsonSettings)
+        public Http(HttpClient client, SlackJsonSettings jsonSettings, bool retryOnRateLimit = true)
         {
             _client = client;
             _jsonSettings = jsonSettings;
+            _retryOnRateLimit = retryOnRateLimit;
         }
 
         public async Task<T> Execute<T>(HttpRequestMessage requestMessage, CancellationToken? cancellationToken = null)
         {
-            var response = await _client.SendAsync(requestMessage, cancellationToken ?? CancellationToken.None).ConfigureAwait(false);
-            response.EnsureSuccessStatusCode();
-            return response.Content.Headers.ContentType.MediaType == "application/json"
-                ? await Deserialize<T>(response).ConfigureAwait(false)
-                : default;
+            while (true)
+            {
+                var response = await _client.SendAsync(requestMessage, cancellationToken ?? CancellationToken.None).ConfigureAwait(false);
+
+                if (_retryOnRateLimit && (int)response.StatusCode == 429) // TODO use the enum when TooManyRequests becomes available
+                {
+                    await Task.Delay(response.Headers.RetryAfter.Delta ?? TimeSpan.FromSeconds(1)).ConfigureAwait(false);
+                }
+                else
+                {
+                    response.EnsureSuccessStatusCode();
+                    return response.Content.Headers.ContentType.MediaType == "application/json"
+                        ? await Deserialize<T>(response).ConfigureAwait(false)
+                        : default;
+                }
+            }
         }
 
         private async Task<T> Deserialize<T>(HttpResponseMessage response) =>
