d36d83e2d98cb1e7e58b46ac31f610a6f53b4932
Simon Oxtoby <soxtoby@zaptechnology.com>
2020-11-07 09:34:35 +1000

    Marking EventCallback.AuthedUsers as Obsolete

--- SlackNet/Events/EventCallback.cs
+++ SlackNet/Events/EventCallback.cs
@@ -12,6 +12,11 @@ namespace SlackNet.Events
         public string TeamId { get; set; }
         public string ApiAppId { get; set; }
         public Event Event { get; set; }
+        /// <summary>
+        /// Will be deprecated by Slack on February 24, 2021.
+        /// See <a href="https://api.slack.com/changelog/2020-09-15-events-api-truncate-authed-users">Slack's changelog page</a> for more information.
+        /// </summary>
+        [Obsolete("Use Authorizations and AppsEventsAuthorizations.List instead")]
         public string[] AuthedUsers { get; set; }
         public IList<Authorization> Authorizations { get; set; }
         public string EventContext { get; set; }
