f6efd342a608a75c3a8cb98fd71865992c766e63
Julian Robinson <julian.robinson@xero.com>
2020-11-07 22:16:38 +1300

    #66 add additional properties to app mention

--- SlackNet/Events/AppMention.cs
+++ SlackNet/Events/AppMention.cs
@@ -1,5 +1,7 @@
 using Newtonsoft.Json;
+using SlackNet.Blocks;
 using System;
+using System.Collections.Generic;
 
 namespace SlackNet.Events
 {
@@ -15,5 +17,14 @@ namespace SlackNet.Events
         public DateTime Timestamp => Ts.ToDateTime().GetValueOrDefault();
         public string Channel { get; set; }
         public string EventTs { get; set; }
+        public string Subtype { get; set; }
+        public string ThreadTs { get; set; }
+        [JsonIgnore]
+        public DateTime? ThreadTimestamp => ThreadTs.ToDateTime();
+        public IList<Attachment> Attachments { get; set; } = new List<Attachment>();
+        public IList<Block> Blocks { get; set; } = new List<Block>();
+        public Edit Edited { get; set; }
+        public IList<File> Files { get; set; } = new List<File>();
+        public bool Upload { get; set; }
     }
 }
