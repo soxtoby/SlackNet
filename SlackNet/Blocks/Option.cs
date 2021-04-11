6d0728ddca851f9e7f0522abd4a368d3d08f11c6
Simon Oxtoby <soxtoby@zaptechnology.com>
2021-04-11 10:02:20 +1000

    Adding Description to block Option

--- SlackNet/Blocks/Option.cs
+++ SlackNet/Blocks/Option.cs
@@ -14,5 +14,10 @@
         /// The string value that will be passed to your app when this option is chosen.
         /// </summary>
         public string Value { get; set; }
+
+        /// <summary>
+        /// A plain text object that defines a line of descriptive text shown below the text field beside a radio button.
+        /// </summary>
+        public PlainText Description { get; set; }
     }
 }
