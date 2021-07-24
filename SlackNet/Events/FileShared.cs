2b7da4f067ed09135abcdccdb107b4a08403bfaa
Martin Johannesson <mjh@it-minds.dk>
2021-07-24 20:06:18 +0200

    Added missing attributes to FileShared event
    According to the event information 
    https://api.slack.com/events/file_shared
    The `user_id` and `channel_id`  are missing.

--- SlackNet/Events/FileShared.cs
+++ SlackNet/Events/FileShared.cs
@@ -6,6 +6,8 @@ namespace SlackNet.Events
     public class FileShared : Event
     {
         public string FileId { get; set; }
+        public string ChannelId { get; set; }
+        public string UserId { get; set; }      
         public FileId File { get; set; }
     }
 }
