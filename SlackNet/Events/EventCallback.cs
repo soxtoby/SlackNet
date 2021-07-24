f35b3d8da4d2921e606c156d6ea4ab394330d9f5
Martin Johannesson <mjh@it-minds.dk>
2021-07-24 20:20:13 +0200

    Added generic version of EventCallback
    Allows for a pre-typed Event property.

--- SlackNet/Events/EventCallback.cs
+++ SlackNet/Events/EventCallback.cs
@@ -19,5 +19,13 @@ namespace SlackNet.Events
         [JsonIgnore]
         public DateTime EventDateTime => EventTime.ToDateTime().GetValueOrDefault();
     }
+    
+    /// <summary>
+    /// Also referred to as the "outer event", or the JSON object containing the event that happened itself.
+    /// </summary>
+    public class EventCallback<T> : EventCallback where T : Event
+    {
+        public new T Event { get; set; }
+    }
 }
 
