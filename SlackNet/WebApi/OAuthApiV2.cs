971e85b7db511504753056fc16f2c2074209b9da
Martin Johannesson <mjh@it-minds.dk>
2021-07-29 11:38:02 +0200

    Implementation of web api oauth.v2.access method and required response type.

---
+++ SlackNet/WebApi/OAuthApiV2.cs
@@ -0,0 +1,51 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Args = System.Collections.Generic.Dictionary<string, object>;
+
+namespace SlackNet.WebApi
+{
+    public interface IOAuthApiV2
+    {
+        /// <summary>
+        /// Exchanges a temporary OAuth verifier code for an access token.
+        /// See https://api.slack.com/methods/oauth.v2.access for more information.
+        /// </summary>
+        /// <param name="clientId">Issued when you created your application.</param>
+        /// <param name="clientSecret">Issued when you created your application.</param>
+        /// <param name="code">The code param returned via the OAuth callback.</param>
+        /// <param name="grant_type">The grant_type param as described in the OAuth spec.</param>
+        /// <param name="redirectUrl">This must match the originally submitted URI (if one was sent).</param>
+        /// <param name="refresh_token">The refresh_token param as described in the OAuth spec.</param>
+        /// <param name="cancellationToken"></param>
+        Task<OauthV2AccessResponse> Access(string clientId, string clientSecret, string code, string grant_type, string redirectUrl, string refresh_token, CancellationToken? cancellationToken = null);
+
+    }
+
+    public class OAuthApiV2 : IOAuthApiV2
+    {
+        private readonly ISlackApiClient _client;
+        public OAuthApiV2(ISlackApiClient client) => _client = client;
+
+        /// <summary>
+        /// Exchanges a temporary OAuth verifier code for an access token.
+        /// See https://api.slack.com/methods/oauth.v2.access for more information.
+        /// </summary>
+        /// <param name="clientId">Issued when you created your application.</param>
+        /// <param name="clientSecret">Issued when you created your application.</param>
+        /// <param name="code">The code param returned via the OAuth callback.</param>
+        /// <param name="grant_type">The grant_type param as described in the OAuth spec.</param>
+        /// <param name="redirectUrl">This must match the originally submitted URI (if one was sent).</param>
+        /// <param name="refresh_token">The refresh_token param as described in the OAuth spec.</param>
+        /// <param name="cancellationToken"></param>
+        public Task<OauthV2AccessResponse> Access(string clientId, string clientSecret, string code, string grantType, string redirectUrl, string refreshToken, CancellationToken? cancellationToken = null) =>
+            _client.Get<OauthV2AccessResponse>("oauth.v2.access", new Args
+                {
+                    { "client_id", clientId },
+                    { "client_secret", clientSecret },
+                    { "code", code },
+                    { "grant_type", grantType },
+                    { "redirect_uri",, redirectUrl },
+                    { "refresh_token", refreshToken }
+                }, cancellationToken);
+    }
+}
---
+++ SlackNet/WebApi/Responses/OauthV2AccessResponse.cs
@@ -0,0 +1,30 @@
+﻿namespace SlackNet.WebApi
+{
+    public class OauthV2AccessResponse
+    {
+        public string AccessToken { get; set; }
+        public string? RefreshToken { get; set; }
+        public int? ExpiredIn { get; set; }
+        public string Scope { get; set; }
+
+        public Space Team { get; set; }
+        public Space Enterprise { get; set; }
+        public User User { get; set; }
+    }
+
+    public class Space
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class User
+    {
+        public string Id { get; set; }
+        public string Scope { get; set; }
+        public string AccessToken { get; set; }
+        public int? ExpiresIn { get; set; }
+        public string? RefreshToken { get; set; }
+        public string TokenType { get; set; }
+    }
+}
