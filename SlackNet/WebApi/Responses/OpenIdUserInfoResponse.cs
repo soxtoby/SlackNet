b5262e31c1234c50fb048a1cea93251c3d1dbe7c
Martin Johannesson <mjh@it-minds.dk>
2021-10-11 11:17:49 +0200

    Made all members public in OpenIdUserInfoResponse

--- SlackNet/WebApi/Responses/OpenIdUserInfoResponse.cs
+++ SlackNet/WebApi/Responses/OpenIdUserInfoResponse.cs
@@ -4,57 +4,57 @@ namespace SlackNet.WebApi
 {
     public class OpenIdUserInfoResponse
     {
-        string Ok { get; set; }
-        string Sub { get; set; }
+        public string Ok { get; set; }
+        public string Sub { get; set; }
 
         [JsonProperty("https://slack.com/user_id")]
-        string UserId { get; set; }
+        public string UserId { get; set; }
 
         [JsonProperty("https://slack.com/team_id")]
-        string TeamId { get; set; }
+        public string TeamId { get; set; }
 
-        string Email { get; set; }
-        string EmailVerified { get; set; }
-        string DateEmailVerified { get; set; }
-        string Name { get; set; }
-        string Picture { get; set; }
-        string GivenName { get; set; }
-        string FamilyName { get; set; }
-        string Locale { get; set; }
+        public string Email { get; set; }
+        public string EmailVerified { get; set; }
+        public string DateEmailVerified { get; set; }
+        public string Name { get; set; }
+        public string Picture { get; set; }
+        public string GivenName { get; set; }
+        public string FamilyName { get; set; }
+        public string Locale { get; set; }
 
         [JsonProperty("https://slack.com/team_name")]
-        string TeamName { get; set; }
+        public string TeamName { get; set; }
         [JsonProperty("https://slack.com/team_domain")]
-        string TeamDomain { get; set; }
+        public string TeamDomain { get; set; }
 
         [JsonProperty("https://slack.com/user_image_24")]
-        string UserImage24 { get; set; }
+        public string UserImage24 { get; set; }
         [JsonProperty("https://slack.com/user_image_32")]
-        string UserImage32 { get; set; }
+        public string UserImage32 { get; set; }
         [JsonProperty("https://slack.com/user_image_48")]
-        string UserImage48 { get; set; }
+        public string UserImage48 { get; set; }
         [JsonProperty("https://slack.com/user_image_72")]
-        string UserImage72 { get; set; }
+        public string UserImage72 { get; set; }
         [JsonProperty("https://slack.com/user_image_192")]
-        string UserImage192 { get; set; }
+        public string UserImage192 { get; set; }
         [JsonProperty("https://slack.com/user_image_512")]
-        string UserImage512 { get; set; }
+        public string UserImage512 { get; set; }
 
         [JsonProperty("https://slack.com/team_image_34")]
-        string TeamImage34 { get; set; }
+        public string TeamImage34 { get; set; }
         [JsonProperty("https://slack.com/team_image_44")]
-        string TeamImage44 { get; set; }
+        public string TeamImage44 { get; set; }
         [JsonProperty("https://slack.com/team_image_68")]
-        string TeamImage68 { get; set; }
+        public string TeamImage68 { get; set; }
         [JsonProperty("https://slack.com/team_image_88")]
-        string TeamImage88 { get; set; }
+        public string TeamImage88 { get; set; }
         [JsonProperty("https://slack.com/team_image_102")]
-        string TeamImage102 { get; set; }
+        public string TeamImage102 { get; set; }
         [JsonProperty("https://slack.com/team_image_132")]
-        string TeamImage132 { get; set; }
+        public string TeamImage132 { get; set; }
         [JsonProperty("https://slack.com/team_image_230")]
-        string TeamImage230 { get; set; }
+        public string TeamImage230 { get; set; }
         [JsonProperty("https://slack.com/team_image_default")]
-        string TeamImageDefault { get; set; }
+        public string TeamImageDefault { get; set; }
     }
 }
