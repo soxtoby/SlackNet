b976f259580bdd5cf9dabf80e6e38f683be16603
Martin Johannesson <mjh@it-minds.dk>
2021-10-11 10:55:46 +0200

    Additional members for openid.connect.token response.

--- SlackNet/WebApi/Responses/OpenIdTokenResponse.cs
+++ SlackNet/WebApi/Responses/OpenIdTokenResponse.cs
@@ -6,5 +6,8 @@
         public string AccessToken { get; set; }
         public string TokenType { get; set; }
         public string IdToken { get; set; }
+        public string State { get; set; }
+        public string RefreshToken { get; set; }
+        public int ExpiresIn { get; set; }
     }
 }
