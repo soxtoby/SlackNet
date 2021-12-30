a94958259ecfe2d3b6b134278af0a8024bfa6630
Kalissaac <kalissaac@protonmail.com>
2021-12-29 16:25:14 -0800

    Add AppUninstalled event content

--- SlackNet/Events/AppUninstalled.cs
+++ SlackNet/Events/AppUninstalled.cs
@@ -1,7 +1,13 @@
-﻿namespace SlackNet.Events
+﻿using System;
+
+namespace SlackNet.Events
 {
     /// <summary>
     /// Your Slack app was uninstalled.
     /// </summary>
-    public class AppUninstalled : Event { }
+    public class AppUninstalled : Event {
+        public string Token { get; set; }
+        public string TeamId { get; set; }
+        public string ApiAppId { get; set; }
+    }
 }
