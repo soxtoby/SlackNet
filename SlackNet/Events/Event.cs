ad870dc76a789307210c49ccf3f29139a6f0cb1a
Simon Oxtoby <simonoxtoby@gmail.com>
2022-05-01 09:56:21 +1000

    Adding ExtraProperties to Event to catch any missing information that Slack sends

--- SlackNet/Events/Event.cs
+++ SlackNet/Events/Event.cs
@@ -1,7 +1,17 @@
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
 namespace SlackNet.Events
 {
     public class Event
     {
         public string Type { get; set; }
+
+        /// <summary>
+        /// Anything that Slack includes in the event that isn't covered by other properties can be accessed through this property.
+        /// If you find anything here, please raise an issue at https://github.com/soxtoby/SlackNet/issues so we can add it to the library.
+        /// </summary>
+        [JsonExtensionData]
+        public Dictionary<string, object> ExtraProperties { get; set; } = new();
     }
 }
