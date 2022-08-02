08f9d9826eebc6d5e01eabead75acd78e85ad9f3
Simon Oxtoby <simonoxtoby@gmail.com>
2022-08-03 08:31:52 +1000

    Removing unused properties from AppUninstalled

--- SlackNet/Events/AppUninstalled.cs
+++ SlackNet/Events/AppUninstalled.cs
@@ -1,13 +1,7 @@
-﻿using System;
-
-namespace SlackNet.Events
+﻿namespace SlackNet.Events
 {
     /// <summary>
     /// Your Slack app was uninstalled.
     /// </summary>
-    public class AppUninstalled : Event {
-        public string Token { get; set; }
-        public string TeamId { get; set; }
-        public string ApiAppId { get; set; }
-    }
+    public class AppUninstalled : Event { }
 }
