081ab36981596ce991f6889c5e80a276be0e25f0
Simon Oxtoby <simonoxtoby@gmail.com>
2022-08-28 10:38:36 +1000

    Updating ViewState name and documentation

--- SlackNet/Objects/ViewInfo.cs
+++ SlackNet/Objects/ViewInfo.cs
@@ -14,7 +14,7 @@ namespace SlackNet
         public string PrivateMetadata { get; set; }
         public string CallbackId { get; set; }
         public string ExternalId { get; set; }
-        public State State { get; set; }
+        public ViewState State { get; set; }
         public string Hash { get; set; }
         public string RootViewId { get; set; }
         public string AppId { get; set; }
