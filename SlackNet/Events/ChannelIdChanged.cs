03537e53d873b459c6d87891c3fb8edbdfdd2bf2
Simon Oxtoby <simonoxtoby@gmail.com>
2022-08-28 11:15:34 +1000

    Adding ChannelIdChanged event

---
+++ SlackNet/Events/ChannelIdChanged.cs
@@ -0,0 +1,8 @@
+﻿namespace SlackNet.Events
+{
+    public class ChannelIdChanged : Event
+    {
+        public string OldChannelId { get; set; }
+        public string NewChannelId { get; set; }
+    }
+}
