38a920062fc93947756cf003b2f294b9cbf92600
Filip Stojanac <filip.stojanac@gmail.com>
2023-07-21 16:18:07 +0200

    [ENH] Adds additional user events

---
+++ SlackNet/Events/UserHuddleChanged.cs
@@ -0,0 +1,9 @@
+namespace SlackNet.Events
+{
+    /// <summary>
+    /// Sent to all connections for a workspace when a user's huddle status is changed.
+    /// </summary>
+    public class UserHuddleChanged : UserChange
+    {
+    }
+}
