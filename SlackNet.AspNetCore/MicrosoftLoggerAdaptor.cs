5f8d6e44837c0798eb46636bf374910db5ea8e54
Simon Oxtoby <simonoxtoby@gmail.com>
2023-08-11 04:35:09 +1000

    ASP.NET logger includes specific SlackNet category in logs
    Allows categories to be enabled separately via logger config

--- SlackNet.AspNetCore/MicrosoftLoggerAdaptor.cs
+++ SlackNet.AspNetCore/MicrosoftLoggerAdaptor.cs
@@ -1,18 +1,20 @@
 ﻿using Microsoft.Extensions.Logging;
 using SlackNetLogger = SlackNet.ILogger;
-using MicrosoftLogger = Microsoft.Extensions.Logging.ILogger;
 
 namespace SlackNet.AspNetCore;
 
 class MicrosoftLoggerAdaptor : SlackNetLogger
 {
-    private readonly MicrosoftLogger _logger;
-    public MicrosoftLoggerAdaptor(ILoggerFactory loggerFactory) => _logger = loggerFactory.CreateLogger("SlackNet");
+    private readonly ILoggerFactory _loggerFactory;
+    public MicrosoftLoggerAdaptor(ILoggerFactory loggerFactory) => _loggerFactory = loggerFactory;
 
     public void Log(ILogEvent logEvent) =>
-        _logger.Log(logEvent.Category switch
+        _loggerFactory
+            .CreateLogger($"SlackNet.{logEvent.Category}")
+            .Log(logEvent.Category switch
                 {
                     LogCategory.Data => LogLevel.Trace,
+                    LogCategory.Serialization => LogLevel.Trace,
                     LogCategory.Internal => LogLevel.Debug,
                     LogCategory.Request => LogLevel.Information,
                     LogCategory.Error => LogLevel.Error,
