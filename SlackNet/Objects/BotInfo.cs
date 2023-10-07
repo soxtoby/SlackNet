e504bddc7fc5eba92b698780825e099d6340c41c
Simon Oxtoby <simonoxtoby@gmail.com>
2023-10-07 10:31:36 +1000

    Adding TeamId, Deleted, and Updated to BotInfo

--- SlackNet/Objects/BotInfo.cs
+++ SlackNet/Objects/BotInfo.cs
@@ -1,10 +1,16 @@
-﻿namespace SlackNet;
+﻿using System;
+
+namespace SlackNet;
 
 public class BotInfo
 {
     public string Id { get; set; }
     public string UserId { get; set; }
     public string AppId { get; set; }
+    public string TeamId { get; set; }
     public string Name { get; set; }
     public Icons Icons { get; set; }
+    public bool Deleted { get; set; }
+    public int Updated { get; set; }
+    public DateTime? UpdatedDateTime => Updated.ToDateTime();
 }
