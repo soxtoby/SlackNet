2a117ee9b4c22dce2379dac6abb0463e87214ff9
rzezak <rzezak@gmail.com>
2023-11-05 12:19:08 +0200

    Write FocusOnLoad only when not default because ExternalMultiSelectMenu does not support it

--- SlackNet/Blocks/SelectMenuBase.cs
+++ SlackNet/Blocks/SelectMenuBase.cs
@@ -12,5 +12,6 @@ public abstract class SelectMenuBase : ActionElement, IInputBlockElement
     /// <summary>
     /// Indicates whether the element will be set to auto focus within the <see cref="ViewInfo"/> object. Only one element can be set to true.
     /// </summary>
+    [IgnoreIfDefault] // See issue #177 - Slack returns an invalid parameter for ExternalMultiSelectMenu
     public bool FocusOnLoad { get; set; }
 }
