a1784dcdfa8458feed9c5f20a97e1cf94fb7cf85
Jeremy Norman <jeremy.norman@re-leased.com>
2023-12-11 08:36:35 +1300

    Add isolated worker example app

---
+++ Examples/AzureIsolatedWorkerFunctionExample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using AzureIsolatedWorkerFunctionExample;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SlackNet.AspNetCore;
+using SlackNet.Events;
+
+var host = new HostBuilder()
+	.ConfigureFunctionsWebApplication()
+	.ConfigureServices(services => {
+			var apiToken = Environment.GetEnvironmentVariable("SlackApiToken", EnvironmentVariableTarget.Process);
+			var signingSecret = Environment.GetEnvironmentVariable("SlackSigningSecret", EnvironmentVariableTarget.Process);
+
+			services.AddSlackNet(c => c
+				// Configure the token used to authenticate with Slack
+				.UseApiToken(apiToken)
+            
+				// Register your Slack handlers here
+				.RegisterEventHandler<MessageEvent, PingDemo>()
+			);
+
+			// This is roughly equivalent to the .UseSlackNet() call in ASP.NET Core
+			services.AddSingleton(new SlackEndpointConfiguration()
+				// The signing secret ensures that SlackNet only handles requests from Slack
+				.UseSigningSecret(signingSecret));
+		})
+	.Build();
+
+host.Run();
---
+++ Examples/AzureIsolatedWorkerFunctionExample/SlackEndpoints.cs
@@ -0,0 +1,34 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.Functions.Worker;
+using SlackNet.AspNetCore;
+
+namespace AzureIsolatedWorkerFunctionExample;
+
+public class SlackEndpoints
+{
+    private readonly ISlackRequestHandler _requestHandler;
+    private readonly SlackEndpointConfiguration _endpointConfig;
+
+    public SlackEndpoints(ISlackRequestHandler requestHandler, SlackEndpointConfiguration endpointConfig)
+    {
+        _requestHandler = requestHandler;
+        _endpointConfig = endpointConfig;
+    }
+
+    [Function("event")]
+    public Task<SlackResult> Event([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequest request) => 
+        _requestHandler.HandleEventRequest(request, _endpointConfig);
+
+    [Function("action")]
+    public Task<SlackResult> Action([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequest request) => 
+        _requestHandler.HandleActionRequest(request, _endpointConfig);
+
+    [Function("options")]
+    public Task<SlackResult> Options([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequest request) => 
+        _requestHandler.HandleOptionsRequest(request, _endpointConfig);
+
+    [Function("command")]
+    public Task<SlackResult> Command([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequest request) => 
+        _requestHandler.HandleSlashCommandRequest(request, _endpointConfig);
+}
