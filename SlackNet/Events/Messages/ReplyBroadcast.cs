599e1a98ea585c4e23743ebeb1941c93324af388
Simon Oxtoby <simonoxtoby@gmail.com>
2024-04-27 10:29:27 +1000

    Adding ThreadBroadcast event
    Deprecating ReplyBroadcast

--- SlackNet/Events/Messages/ReplyBroadcast.cs
+++ SlackNet/Events/Messages/ReplyBroadcast.cs
@@ -1,6 +1,9 @@
-﻿namespace SlackNet.Events;
+﻿using System;
+
+namespace SlackNet.Events;
 
 /// <summary>
 /// Sent when a user or bot user has indicated their reply should be broadcast to the whole channel.
 /// </summary>
+[Obsolete("Use ThreadBroadcast instead.")]
 public class ReplyBroadcast : MessageEvent;
---
+++ SlackNet/Events/Messages/ThreadBroadcast.cs
@@ -0,0 +1,11 @@
+﻿namespace SlackNet.Events;
+
+/// <summary>
+/// The <see cref="ThreadBroadcast"/> message subtype is sent when a user or bot user has indicated their reply should be broadcast to the whole channel.
+/// It's a pointer or reference to the actual thread and is meant more to be informational than to fully describe the message.
+/// The reference cannot contain attachments or message buttons.
+/// </summary>
+public class ThreadBroadcast : ReplyBroadcast
+{
+    public MessageEvent Root { get; set; }
+}
