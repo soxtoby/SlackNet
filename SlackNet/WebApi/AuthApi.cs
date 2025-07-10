7efb174d091af6d405d9c412d128a35db71d1cbd
ynaftali-island <yonatan.naftali@island.io>
2025-07-10 14:39:43 +0300

    feat(auth): Created TeamsList Request

--- SlackNet/WebApi/AuthApi.cs
+++ SlackNet/WebApi/AuthApi.cs
@@ -22,6 +22,16 @@ public interface IAuthApi
     /// <remarks>See the <a href="https://api.slack.com/methods/auth.test">Slack documentation</a> for more information.</remarks>
     /// <param name="cancellationToken"></param>
     Task<AuthTestResponse> Test(CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Obtain a full list of workspaces your org-wide app has been approved for.
+    /// </summary>
+    /// <remarks>See the <a href="https://api.slack.com/methods/auth.teams.list">Slack documentation</a> for more information.</remarks>
+    /// <param name="cursor">Set cursor to next_cursor returned by the previous call to list items in the next page.</param>
+    /// <param name="includeIcon">Whether to return icon paths for each workspace. An icon path represents a URI pointing to the image signifying the workspace.</param>
+    /// <param name="limit">The maximum number of workspaces to return. Must be a positive integer no larger than 1000.</param>
+    /// <param name="cancellationToken"></param>
+    Task<AuthTeamsListResponse> TeamsList(string cursor = null, bool includeIcon = false, int limit = 100, CancellationToken cancellationToken = default);
 }
 
 public class AuthApi : IAuthApi
@@ -34,4 +44,12 @@ public class AuthApi : IAuthApi
 
     public Task<AuthTestResponse> Test(CancellationToken cancellationToken = default) =>
         _client.Post<AuthTestResponse>("auth.test", new Args(), cancellationToken);
+
+    public Task<AuthTeamsListResponse> TeamsList(string cursor = null, bool includeIcon = false, int limit = 100, CancellationToken cancellationToken = default) =>
+        _client.Get<AuthTeamsListResponse>("auth.teams.list", new Args
+            {
+                { "cursor", cursor },
+                { "include_icon", includeIcon },
+                { "limit", limit }
+            }, cancellationToken);
 }
