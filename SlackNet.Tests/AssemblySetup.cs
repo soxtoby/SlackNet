abb2bd087e7713e874d07ab7c5c2bee60de3d2ad
Simon Oxtoby <simonoxtoby@gmail.com>
2025-07-13 10:47:24 +1000

    Configurating NUnit exception for EasyAssertions

---
+++ SlackNet.Tests/AssemblySetup.cs
@@ -0,0 +1,14 @@
+﻿using EasyAssertions;
+using NUnit.Framework;
+
+namespace SlackNet.Tests;
+
+[SetUpFixture]
+public class AssemblySetup
+{
+    [OneTimeSetUp]
+    public void SetUp()
+    {
+        EasyAssertion.UseFrameworkExceptions(m => new AssertionException(m), (m, e) => new AssertionException(m, e));
+    }
+}
